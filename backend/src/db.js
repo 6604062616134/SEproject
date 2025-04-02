d8141750e4525e7b6fec71da9b5375a89329695e
creamp_ <pattarawan654@gmail.com>
2025-04-02 10:58:15 +0700

    .

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,39 +1,39 @@
-// const mysql = require('mysql2/promise');
-
-// const pool = mysql.createPool({
-//     host: 'localhost', 
-//     user: 'root', 
-//     password: '', 
-//     database: 'boardgames', 
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-
-// module.exports = pool;
-
-require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, // ชื่อ host (db)
-    user: process.env.DB_USER, // ชื่อผู้ใช้ (root)
-    password: process.env.DB_PASSWORD, // รหัสผ่าน (root)
-    database: process.env.DB_NAME, // ชื่อฐานข้อมูล (boardgames)
+    host: 'localhost', 
+    user: 'root', 
+    password: '', 
+    database: 'boardgames', 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// ทดสอบการเชื่อมต่อ
-(async () => {
-    try {
-        const connection = await pool.getConnection();
-        console.log('Connected to the database successfully!');
-        connection.release();
-    } catch (error) {
-        console.error('Error connecting to the database:', error);
-    }
-})();
-
 module.exports = pool;
+
+// require('dotenv').config();
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST, // ชื่อ host (db)
+//     user: process.env.DB_USER, // ชื่อผู้ใช้ (root)
+//     password: process.env.DB_PASSWORD, // รหัสผ่าน (root)
+//     database: process.env.DB_NAME, // ชื่อฐานข้อมูล (boardgames)
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+// // ทดสอบการเชื่อมต่อ
+// (async () => {
+//     try {
+//         const connection = await pool.getConnection();
+//         console.log('Connected to the database successfully!');
+//         connection.release();
+//     } catch (error) {
+//         console.error('Error connecting to the database:', error);
+//     }
+// })();
+
+// module.exports = pool;
